44fd506778c1b3534dc872feb860580154abe384
furkanbagci34 <62967565+furkanbagci34@users.noreply.github.com>
2023-04-06 23:55:34 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,17 @@ import ElementPlus from "element-plus";
 import i18n from "@/core/plugins/i18n";
 import { initInlineSvg } from "@/core/plugins/inline-svg";
 import { initVeeValidate } from "@/core/plugins/vee-validate";
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 initInlineSvg(app);
 initVeeValidate();
--- src/views/operation.vue
+++ src/views/operation.vue
@@ -1,39 +1,59 @@
 <template>
-  <div class="demo-collapse">
-    <el-collapse accordion v-for="op in operationList" v-model="activeOperation" @change="OperationChange(op.status)">
-      
-      <el-collapse-item :name="op.operationId">
+  <el-card shadow="always">
+    <div class="demo-collapse">
+      <el-collapse accordion v-for="op in operationList" v-model="activeOperation" @change="operationChange(op.status,op.operationId)">
         
-        <template #title>
-          <div v-if="op.status == 0">
-            <span class="fw-bolder fs-4">{{ op.operationName }}</span>&nbsp;&nbsp;
-            <i class="bi bi-hourglass-top" style="font-size: 1.75rem; color: red;"></i>
-          </div>
+        <el-collapse-item :name="op.operationId">
+          
+          <template #title>
+            <div v-if="op.status == 0">
+              <span class="fw-bolder fs-4">{{ op.operationName }}</span>&nbsp;&nbsp;
+              <i class="bi bi-hourglass-top" style="font-size: 1.75rem; color: red;"></i>
+            </div>
 
-          <div v-if="op.status == 1">
-            <span class="fw-bolder fs-4">{{ op.operationName }}</span>&nbsp;&nbsp;
-            <i class="bi bi-wrench-adjustable-circle-fill" style="font-size: 1.75rem; color: orange;"></i>
-          </div>
+            <div v-if="op.status == 1">
+              <span class="fw-bolder fs-4">{{ op.operationName }}</span>&nbsp;&nbsp;
+              <i class="bi bi-wrench-adjustable-circle-fill" style="font-size: 1.75rem; color: orange;"></i>
+            </div>
 
-          <div v-if="op.status == 2">
-            <span class="fw-bolder fs-4">{{ op.operationName }}</span>&nbsp;&nbsp;
-            <i class="bi bi-check-circle-fill" style="font-size: 1.75rem; color: green;"></i>
-          </div>
-        </template>
+            <div v-if="op.status == 2">
+              <span class="fw-bolder fs-4">{{ op.operationName }}</span>&nbsp;&nbsp;
+              <i class="bi bi-check-circle-fill" style="font-size: 1.75rem; color: green;"></i>
+            </div>
+          </template>
 
-        <el-radio-group v-model="operationStatus">
-          <el-radio :label="0">Bekliyor</el-radio>
-          <el-radio :label="1">Yapılıyor</el-radio>
-          <el-radio :label="2">Tamamlandı</el-radio>
-        </el-radio-group>
-        <button class="btn btn-success btn-sm btn-xsm mb-3" @click="OperationStatusSave">
-        <span class="bi bi-check-lg align-items-center fs-6"> Kaydet</span></button> 
-        <br>
-        
+          <el-card shadow="always" style="background-color: azure;">
+            <el-radio-group class="mt-2" v-model="operationStatus">
+              <el-radio :label="0">Bekliyor</el-radio>
+              <el-radio :label="1">Yapılıyor</el-radio>
+              <el-radio :label="2">Tamamlandı</el-radio>
+            </el-radio-group>
+            <button class="btn btn-success btn-sm btn-xsm mb-3" @click="operationStatusSave">
+            <span class="bi bi-check-lg align-items-center fs-6"> Kaydet</span></button> 
+          </el-card>
+          <br>
 
-      </el-collapse-item>
-    </el-collapse>
-  </div>
+          <el-upload
+            v-model:file-list="fileList"
+            action="''"
+            :multiple="true"
+            list-type="picture-card"
+            :auto-upload="false"
+            :on-change="convertToBase64"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+          >
+            <el-icon><Plus /></el-icon>
+          </el-upload>
+
+          <el-dialog v-model="dialogVisible" width="90%" class="text-center">
+            <img w-full :src="dialogImageUrl" alt="Preview Image" class="img-fluid"/>
+          </el-dialog>
+
+        </el-collapse-item>
+      </el-collapse>
+    </div>
+  </el-card>
 </template>
 
 <script lang="ts">
@@ -41,6 +61,8 @@ import DevPlusApiService from "@/core/services/ApiServiceDevPlus";
 import JwtService from "@/core/services/JwtService";
 import { defineComponent, ref, onMounted } from "vue";
 import { useRouter, useRoute  } from "vue-router";
+import { Plus } from '@element-plus/icons-vue'
+import type { UploadProps, UploadUserFile } from 'element-plus'
 import Swal from "sweetalert2";
 
 export default defineComponent({
@@ -48,6 +70,24 @@ export default defineComponent({
     components: {
 
     },
+    
+    methods: {
+      async convertToBase64(file) {
+        const base64: any = await this.fileToBase64(file.raw)
+
+        this.photoList.push(base64)
+        this.operationPhotoCreate(this.photoList)
+      },  
+      fileToBase64(file) {
+        return new Promise((resolve, reject) => {
+          const reader = new FileReader()
+          reader.readAsDataURL(file)
+          reader.onload = () => resolve(reader.result)
+          reader.onerror = error => reject(error)
+        })
+      }
+    },
+
     setup () {
       const ApiService = new DevPlusApiService();
       const router = useRouter();
@@ -55,12 +95,33 @@ export default defineComponent({
       const operationList = ref();
       const activeOperation = ref()
       const operationStatus = ref()
+      const dialogImageUrl = ref('')
+      const dialogVisible = ref(false)
+      const fileList = ref<UploadUserFile[]>([])
+      const photoList = ref<string[]>([])
 
-      const OperationChange = (opStatus: any) => {
+      const operationChange = async (opStatus: any, opId: any) => {
+        
+        fileList.value = []
         operationStatus.value = opStatus
+
+        if(!activeOperation.value) return;
+
+        const vehicleId = typeof route.params.id === 'string' ? parseInt(route.params.id) : route.params.id[0];
+        const data = await ApiService.Post("vehicle/operationPhotoGet", { vehicleId: vehicleId, operationId: activeOperation.value,}, JwtService.getToken());
+        console.log(data)
+
+        if(data.success && data.success == true){
+
+          data.body.forEach(i => {
+            fileList.value.push({name: i.id, url: i.photo})
+            photoList.value.push(i.photo)
+          });
+        }
+        console.log(photoList)
       }
 
-      const OperationStatusSave = async () => {
+      const operationStatusSave = async () => {
 
         const vehicleId = typeof route.params.id === 'string' ? parseInt(route.params.id) : route.params.id[0];
         const data = await ApiService.Post("vehicle/operationUpdate", { vehicleId: vehicleId, operationId: activeOperation.value, status: operationStatus.value }, JwtService.getToken());
@@ -71,6 +132,24 @@ export default defineComponent({
         }
       }
 
+      const operationPhotoCreate = async (photoList: any) => {
+
+        const vehicleId = typeof route.params.id === 'string' ? parseInt(route.params.id) : route.params.id[0];
+        const data = await ApiService.Post("vehicle/operationPhotoCreate", { vehicleId: vehicleId, operationId: activeOperation.value, photoList: photoList }, JwtService.getToken());
+
+        console.log(data)
+      }
+
+      const handleRemove: UploadProps['onRemove'] = (uploadFile, uploadFiles) => {
+        console.log(uploadFile.name)
+        
+      }
+
+      const handlePictureCardPreview: UploadProps['onPreview'] = (uploadFile) => {
+        dialogImageUrl.value = uploadFile.url!
+        dialogVisible.value = true
+      }
+
       onMounted(async () =>{
         const vehicleId = typeof route.params.id === 'string' ? parseInt(route.params.id) : route.params.id[0];
         const data = await ApiService.Post("vehicle/operationGet", { vehicleId: vehicleId }, JwtService.getToken());
@@ -79,10 +158,17 @@ export default defineComponent({
 
       return{
         operationList,
+        fileList,
+        photoList,
         activeOperation,
-        OperationChange,
         operationStatus,
-        OperationStatusSave
+        dialogVisible,
+        dialogImageUrl,
+        operationChange,
+        operationStatusSave,
+        handleRemove,
+        handlePictureCardPreview,
+        operationPhotoCreate
       }
     }
 })
