5532255bb4c476e2b7258cdde69a1252062ea2dc
Mahir Gündüz <mhrgndz171@gmail.com>
2023-04-08 21:57:45 +0300

    .

--- src/views/myVehicle.vue
+++ src/views/myVehicle.vue
@@ -85,18 +85,3 @@ export default defineComponent({
     }
 })
 </script>
-
-<style scoped>
-    .demo-image__lazy {
-    height: 400px;
-    overflow-y: auto;
-    }
-    .demo-image__lazy .el-image {
-    display: block;
-    min-height: 200px;
-    margin-bottom: 10px;
-    }
-    .demo-image__lazy .el-image:last-child {
-    margin-bottom: 0;
-    }
-</style>
